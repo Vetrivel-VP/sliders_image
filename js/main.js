ba90c677668c94355cf4914ecf3ec6dd7a3aa7c3
Vetrivel-VP <vetrivel.galaxy@gmail.com>
2021-10-08 12:35:34 +0300

    Add : Creative Sliders

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".myswiper1", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+});
+
+var swiper2 = new Swiper(".myswiper2", {
+  effect: "cards",
+  grabCursor: true,
+});
